b7a1a87bc77a8f6064b70abda67a84bbdccc89ce
daeuneeee <dxexx2@gmail.com>
2022-12-06 18:06:26 +0900

    :lipstick: (layout): STYLE: 레이아웃 분기
    레이아웃 분기
    
    closed #31

--- pages/admin/home/index.tsx
+++ pages/admin/home/index.tsx
@@ -1,12 +1,7 @@
-import AdminLayout from '../../../src/components/commons/layoutadmin';
 import HomeContainer from '../../../src/components/units/admin/home/home.container';
 
 const AdminHomePage = () => {
-  return (
-    <AdminLayout>
-      <HomeContainer />
-    </AdminLayout>
-  );
+  return <HomeContainer />;
 };
 
 export default AdminHomePage;
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,17 +1,12 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import AdminLayout from "../../src/components/commons/layoutadmin";
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const AdminPage = () => {
   const router = useRouter();
   useEffect(() => {
-    router.push("/admin/home");
+    router.push('/admin/home');
   }, []);
-  return (
-    <AdminLayout>
-      <div>안녕해새요~ 어떻게 들어오션나용</div>
-    </AdminLayout>
-  );
+  return <div>안녕해새요~ 어떻게 들어오션나용</div>;
 };
 
 export default AdminPage;
