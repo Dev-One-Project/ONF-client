030ba2b976557d0acdc731f9a7680f263e4ef704
idealstring <idealstring@gmail.com>
2022-12-09 11:07:15 +0900

    :sparkles: (adminRequest) FEATURE: 관리자페이지 요청내역 퍼블리싱 #48
    관리자 요청내역 퍼블리싱
    
    closed #48

---
+++ pages/admin/request/index.tsx
@@ -0,0 +1,7 @@
+import RequestContainer from '../../../src/components/units/admin/request/request.container';
+
+const AdminRequestPage = () => {
+  return <RequestContainer />;
+};
+
+export default AdminRequestPage;
--- src/components/commons/input/check01.tsx
+++ src/components/commons/input/check01.tsx
@@ -54,6 +54,7 @@ const Checkbox = styled.span`
 `;
 
 export const Label = styled.label`
+  width: 3rem;
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
