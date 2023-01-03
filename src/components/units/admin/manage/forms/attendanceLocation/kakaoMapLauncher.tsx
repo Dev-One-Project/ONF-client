9320fd3846d1de799caf6416066c9cc1682fe3ba
aggie97 <4ggie1997@gmail.com>
2023-01-03 17:01:20 +0900

    :sparkles: (adminManage): FEATURE: 출퇴근 장소 form daum-post-code 추가
    출퇴근 장소 form daum-post-code 추가
    
    출퇴근 장소 form register 연결 완료 (장소명, 좌표, wifi, 메모)
    
    closed #169

--- src/components/units/admin/manage/forms/attendanceLocation/kakaoMapLauncher.tsx
+++ src/components/units/admin/manage/forms/attendanceLocation/kakaoMapLauncher.tsx
@@ -15,9 +15,7 @@ interface IKakaoMapProps {
     SetStateAction<Partial<GeolocationCoordinates | undefined>>
   >;
   markerPosition?: Partial<GeolocationCoordinates>;
-  setMarkerPosition: Dispatch<
-    SetStateAction<Partial<GeolocationCoordinates | undefined>>
-  >;
+  setMarkerPosition: (args: Partial<GeolocationCoordinates>) => void;
   radius: valueType | null;
   address: string;
 }
@@ -37,6 +35,26 @@ const KakaoMapLauncher = (props: IKakaoMapProps) => {
       const { kakao } = window;
 
       kakao.maps.load(() => {
+        // const geocoder = new kakao.maps.services.Geocoder();
+        // // 주소로 좌표를 검색합니다
+        // geocoder.addressSearch(address, function (result, status) {
+        //   // 정상적으로 검색이 완료됐으면
+        //   if (status === kakao.maps.services.Status.OK) {
+        //     const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+        //     // 결과값으로 받은 위치를 마커로 표시합니다
+        //     // props.setCurrentPosition({
+        //     //   latitude: coords.Ma,
+        //     //   longitude: coords.La,
+        //     // });
+        //     // props.setMarkerPosition({
+        //     //   latitude: coords.Ma,
+        //     //   longitude: coords.La,
+        //     // });
+        //     // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        //   }
+        // });
+
         const container = document.getElementById('map');
 
         const options = {
@@ -49,22 +67,11 @@ const KakaoMapLauncher = (props: IKakaoMapProps) => {
 
         const map = new kakao.maps.Map(container, options);
 
-        // const imageSrc = '/marker/marker.png'; // 마커이미지의 주소입니다
-        // const imageSize = new kakao.maps.Size(45, 45); // 마커이미지의 크기입니다
-        // const imageOption = { offset: new kakao.maps.Point(20, 40) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-        // // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-        // const markerImage = new kakao.maps.MarkerImage(
-        //   imageSrc,
-        //   imageSize,
-        //   imageOption,
-        // );
         const markerPosition = new kakao.maps.LatLng(
           props.markerPosition?.latitude,
           props.markerPosition?.longitude,
-        ); // 마커가 표시될 위치입니다
+        );
 
-        // 마커를 생성합니다
         const marker = new kakao.maps.Marker({
           map,
           position: markerPosition,
@@ -73,8 +80,6 @@ const KakaoMapLauncher = (props: IKakaoMapProps) => {
 
         kakao.maps.event.addListener(marker, 'dragend', () => {
           props.setMarkerPosition({
-            // marker.getPosition().Ma,
-            // marker.getPosition().La,
             latitude: marker.getPosition().Ma,
             longitude: marker.getPosition().La,
           });
@@ -109,7 +114,6 @@ const KakaoMapLauncher = (props: IKakaoMapProps) => {
           const level = map.getLevel();
           setMapLevel(level);
         });
-        // const ps = new kakao.maps.services.Places();
       });
     };
   }, [props, mapLevel]);
